c085466bf0a3c548febc12df0d29ac77337ea201
Swetha Janardhana <SwethaJanardhana85@gmail.com>
2024-03-05 08:31:49 +0100

    bug fix

--- components/ArtPieceDetails.js
+++ components/ArtPieceDetails.js
@@ -78,7 +78,7 @@ export default function ArtPieceDetails({
         <li>
           <StyledDiv
             $color={"white"}
-            onClick={() => handleBackground("#000000")}
+            onClick={() => onBackgroundSelection(slug, "#000000")}
           ></StyledDiv>
         </li>
       </StyledList>
